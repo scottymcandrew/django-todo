90fb1de19702eabd13f406611716827b63373d44
Scott McAndrew <scottymcandrew@outlook.com>
2020-02-17 11:17:21 +0000

    Typo in allowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 # Make sure to use your own hostname here, and/or use`127.0.0.1` for
 # localhost
-ALLOWED_HOSTS = ['127.0.0.1', 'django-todo-richardadalton.c9users.io', 'https://sdm-simple-django-todo.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1', 'django-todo-richardadalton.c9users.io', 'sdm-simple-django-todo.herokuapp.com']
 
 # Application definition
 
