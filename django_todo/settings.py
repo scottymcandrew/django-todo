34f39c5c063a4692bdb36f7a9f5ef056f862150a
Scott McAndrew <scottymcandrew@outlook.com>
2020-02-17 10:31:33 +0000

    Updated settings and requirements

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -28,7 +28,6 @@ DEBUG = True
 # Make sure to use your own hostname here, and/or use`127.0.0.1` for
 # localhost
 ALLOWED_HOSTS = ['127.0.0.1', 'django-todo-richardadalton.c9users.io']
-
 
 # Application definition
 
@@ -72,17 +71,20 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'django_todo.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
+DATABASES = {
+    'default': dj_database_url.parse(
+        "postgres://tomapkpwzgespa:832fa98b3fff1ce0b5b1e5da766592946b04e5ddbc3d2d5487082f72a3728d7d@ec2-54-247-125-38.eu-west-1.compute.amazonaws.com:5432/ddj1qr68nlck00")
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -102,7 +104,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
@@ -115,7 +116,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
